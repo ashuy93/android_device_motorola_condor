d8891dab8fb5c0ba4e2ddf115630abd8ab48be5b
Dashaanan <ashuy93@gmail.com>
2017-01-01 08:17:22 +0000

    validus

--- validus.mk
+++ validus.mk
@@ -1,8 +1,8 @@
 $(call inherit-product, device/motorola/condor/full_condor.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tipsy/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := tipsy_condor
+PRODUCT_NAME := validus_condor
 PRODUCT_RELEASE_NAME := MOTO E
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo tipsy_condor-userdebug
+add_lunch_combo validus_condor-userdebug
